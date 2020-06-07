7e1564ea4b9d410a6671584ded327df95c9462e6
Anastasia <anastasia_telegina@mail.ru>
2020-06-08 00:43:38 +0500

    print time

---
+++ ntp.py
@@ -0,0 +1,14 @@
+import socket
+import ntplib
+from time import ctime
+
+
+
+def print_time():
+    ntpClient = ntplib.NTPClient()
+    response = ntpClient.request('pool.ntp.org')
+
+    print(ctime(response.tx_time))
+    
+if __name__ == "__main__":
+    print_time()
